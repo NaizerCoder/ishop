80f8831013c0535560a8be02fe6aa126f08a18fb
NaizerHome <mr.beensir@yandex.ru>
2024-06-23 01:17:33 +0300

    H:23.06.2024

--- app/Http/Controllers/API/Product/IndexController.php
+++ app/Http/Controllers/API/Product/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\Product;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Product\IndexProductResource;
 use App\Http\Resources\Product\ProductResource;
 use App\Models\Product;
 
@@ -11,6 +12,6 @@ class IndexController extends Controller
     public function __invoke()
     {
         $products = Product::all();
-        return ProductResource::collection($products);
+        return IndexProductResource::collection($products);
     }
 }
---
+++ app/Http/Resources/Product/IndexProductResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use App\Http\Resources\Category\CategoryResource;
+use App\Http\Resources\Image\ImageResource;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class IndexProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'description' => $this->description,
+            'image' => ImageResource::collection($this->images),
+            'price' => $this->price,
+            'old_price' => $this->old_price,
+            'count' => $this->count,
+            'is_publish' => $this->is_publish,
+            'category' => new CategoryResource($this->category),
+
+        ];
+    }
+}
